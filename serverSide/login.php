dbe08641631227ad6fa2f56c5de3569b20f5289c
Muhanna jamal mohammed sabt ahmed <muhannajs@gmail.com>
2024-11-30 03:34:08 +0300

    Update login.php

--- serverSide/login.php
+++ serverSide/login.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
-        header('Location: index.php');
+        header('Location: index.php'); // Redirect to the home page
         exit();
     } else {
         echo 'Invalid email or password.';
