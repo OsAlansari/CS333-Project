e23c4621fb8ad441f40cb3302115d0f230042843
OsAlansari <149081443+OsAlansari@users.noreply.github.com>
2024-12-08 10:14:54 +0300

    cleaning up the files

--- serverSide/login-register.php
+++ serverSide/login-register.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Css/style.css">
+    <link rel="stylesheet" href="../Css/login-register.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <title>Login/Register</title>
 </head>
