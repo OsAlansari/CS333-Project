0cd79bb8219de15d0dde98de1daeb868463a379c
OsAlansari <149081443+OsAlansari@users.noreply.github.com>
2024-12-10 15:09:35 +0300

    ..

--- serverSide/admin.php
+++ serverSide/admin.php
@@ -4,7 +4,7 @@ session_start();
 include 'config.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: /login-register.php');
+    header('Location: ../serverSide/login-register.php');
     exit();
 }
 
@@ -15,7 +15,7 @@ $stmt->execute([$user_id]);
 $type = $stmt->fetch();
 
 if ($type['user_type'] != 'Admin') {
-    header('Location: index.php');
+    header('Location: ../serverSide/index.php');
     exit();
 }
 
